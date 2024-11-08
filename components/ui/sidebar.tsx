0822fe780d85153f8c3bfebe7593ab5dfa309c6d
RohithAchar <rohithachar2000@gmail.com>
2024-11-08 12:40:49 +0530

    fixed border in mobile

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -204,7 +204,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] rounded-[20px] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] rounded-tr-[20px] rounded-br-[20px] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
